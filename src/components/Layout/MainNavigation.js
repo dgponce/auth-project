7c43c3a0eb5a786122ac7531c5233c78a3c56632
dgponce <dg38828@gmail.com>
2022-12-12 11:56:07 -0600

    Added guard routing

--- src/components/Layout/MainNavigation.js
+++ src/components/Layout/MainNavigation.js
@@ -8,6 +8,10 @@ const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
   const isLoggedIn = authCtx.isLoggedIn;
 
+  const logOutHandler = () => {
+    authCtx.logout();
+  }
+
   return (
     <header className={classes.header}>
       <Link to='/'>
@@ -27,7 +31,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button>Logout</button>
+              <button onClick={logOutHandler}>Logout</button>
             </li>
           )}
         </ul>
